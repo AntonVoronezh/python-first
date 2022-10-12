3c32faf6d4665411b5931a95f2783a65cfe01d39
AntonVoronezh <pstut@mail.ru>
2022-10-12 18:11:54 +0300

    7

---
+++ lesson7.py
@@ -0,0 +1,27 @@
+test = None
+
+print(test)
+
+x = 1;
+y = 5
+
+z, t = (1, 2)
+
+print(z, t)
+
+my_true = True
+my_false = False
+
+print(type(my_true))
+
+aa = 1
+aa_str = str(aa)
+aa_int = int(aa)
+aa_float = float(aa)
+aa_bool = bool(aa)
+
+print(aa_str)
+print(aa_int)
+print(aa_float)
+print(aa_bool)
+
