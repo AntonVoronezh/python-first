4c6d94d9fdb19f730bd78bb0f65249fdf7f1072f
AntonVoronezh <pstut@mail.ru>
2022-10-13 18:20:08 +0300

    10

---
+++ lesson10.py
@@ -0,0 +1,18 @@
+s = 'tuiyYotW79 kyvi'
+
+print(len(s))
+
+print(s.capitalize()) # Tuiyyotw79
+
+print(s.center(20, '*')) # *****tuiyYotW79*****
+
+print(s.count('t')) # 2
+
+print(s.find('t')) # 0 - позиция первого вхождения, если нет то ошибка
+print(s.replace('t', '*')) # *uiyYo*W79
+print(s.split()) # ['tuiyYotW79', 'kyvi']
+print(s.isdigit()) # False
+
+
+
+
