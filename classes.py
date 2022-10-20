cdec58310e7436563858027d4eb2a21fed4bcbbd
AntonVoronezh <pstut@mail.ru>
2022-10-20 09:46:46 +0300

    37

---
+++ classes.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def print_info(self):
+        print('hello, aim ' + self.name)
---
+++ lesson37.py
@@ -0,0 +1,8 @@
+from classes import Person
+# import classes
+
+pers1 = Person('ssss')
+pers2 = Person('jjjj')
+
+pers1.print_info()
+pers2.print_info()
