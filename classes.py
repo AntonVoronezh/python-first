589fa1d2e65f260bafd49c3100b28eb811fb2362
AntonVoronezh <pstut@mail.ru>
2022-10-20 17:26:41 +0300

    38

--- classes.py
+++ classes.py
@@ -1,6 +1,25 @@
 class Person:
     def __init__(self, name):
         self.name = name
+        self._age = 20  # private
+        self.__age2 = 10  # private
 
     def print_info(self):
-        print('hello, aim ' + self.name)
+        print(f'Name: {self.name} + Age: {self._age} + Age2: {self.__age2}')
+
+    def get_age(self):
+        return self.__age2
+
+    def set_age(self, age):
+        if age in range(1, 101):
+            self.__age2 = age
+        else:
+            self.__age2 = 99
+
+    @property  # геттер декоратор
+    def age(self):
+        return self._age
+
+    @age.setter  # сеттер декоратор
+    def age(self, val):
+        self._age = val
---
+++ lesson38.py
@@ -0,0 +1,14 @@
+from classes import Person
+
+a = Person('Fnd4ny5sr')
+
+a.print_info()
+# print(a._Person__age2)
+
+# print(a.get_age())
+a.set_age(100)
+a.print_info()
+
+print(f'decor - {a.age}')
+a.age = 77
+print(f'decor - {a.age}')
