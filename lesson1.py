bcd59a27edab69e2c556d5e4077141f79bab7ac0
AntonVoronezh <pstut@mail.ru>
2022-10-21 19:32:12 +0300

    1

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,13 @@
-import zipfile
-import os
+from tkinter import *
+
+root = Tk()
+root.title('кдгнвег 6кнлвекг7')
+# root.iconbitmap('ffff.ico')
+root.geometry('600x400+500+250')
+root.resizable(False, False)
+# root.config(bg='red')
+# root['bg'] = 'red'
+
+
+root.mainloop()
 
