e3e13496cd33cc89e7fd89371b5c511fc8a89a03
AntonVoronezh <pstut@mail.ru>
2022-10-21 18:18:36 +0300

    46

--- lesson45.py
+++ lesson45.py
@@ -4,12 +4,25 @@ import sqlite3
 db = sqlite3.connect('test_db.sqlite')
 cursor = db.cursor()
 
-cursor.execute('''
-CREATE TABLE users (
-id INTEGER PRIMARY KEY, 
-name TEXT NOT NULL, 
-email TEXT NOT NULL UNIQUE
-)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXIST users (
+# id INTEGER PRIMARY KEY,
+# name TEXT NOT NULL,
+# email TEXT NOT NULL UNIQUE
+# )
+# ''')
+
+# cursor.execute("INSERT INTO users (name, email) VALUES ('Иванов', 'grhy@jyu')")
+# cursor.execute("INSERT INTO users (name, email) VALUES ('Петров', 'kvhilgul@jyu')")
+
+# cursor.executescript('''
+# INSERT INTO users (name, email) VALUES ('khi', 'ukh@jyu');
+# INSERT INTO users (name, email) VALUES ('ukgiul', 'uiyiugiy@jyu');
+# ''')
+users = [('us1', 'usfkfl@lh;h;'), ('us2', 'yutudt@kkk')]
+cursor.executemany("INSERT INTO users (name, email) VALUES (?, ?)", users)
+
+db.commit()
+
 
 db.close()
